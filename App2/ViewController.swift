4535cecfdb5ba5df99ad2ad68d4ddb3725c838c3
robinbruer <r_bruer@hotmail.com>
2019-04-15 12:08:29 +0200

    Initial Commit

---
+++ App2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App2
+//
+//  Created by Robin on 15/04/2019.
+//  Copyright © 2019 Robin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
